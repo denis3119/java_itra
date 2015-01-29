407f8a73443b8a326f9606ece3774e9fb1cfd664
denis3119 <denis3119@gmail.com>
2015-01-30 01:34:08 +0300

    f

--- src/java_itra/Program.java
+++ src/java_itra/Program.java
@@ -1,12 +1,19 @@
 package java_itra;
 
+<<<<<<< HEAD
 
 
+=======
+import Hiberante.HibernateUtilites;
+
+import org.hibernate.Session;
+>>>>>>> parent of 8e981a5... Revert "123"
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+<<<<<<< HEAD
 		System.out.println("12312321");//		UserManager manager = new UserManager();
 //		manager.Create(user);
 //		manager.Create(user);
@@ -14,6 +21,10 @@ public class Program {
 //		manager.Create(user);
 //		manager.Create(user);
 //		manager.editUserName(7,"7");
+=======
+		System.out.println("12312321");
+
+>>>>>>> parent of 8e981a5... Revert "123"
 
 	}
 
