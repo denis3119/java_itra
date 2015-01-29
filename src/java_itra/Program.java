7349d95fd21af32f74c805011c5d8fcd5065fa13
denis3119 <denis3119@gmail.com>
2015-01-30 01:33:33 +0300

    1

--- src/java_itra/Program.java
+++ src/java_itra/Program.java
@@ -1,26 +1,19 @@
 package java_itra;
 
-import Hiberante.HibernateUtilites;
-import Managers.UserManager;
-import Models.User;
-import org.hibernate.Session;
+
+
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("12312321");
-		User user= new User();
-		user.setUsername("new_name");
-		user.setPassword("pass");
-		user.setEnabled(true);
-		UserManager manager = new UserManager();
-		manager.Create(user);
-		manager.Create(user);
-		manager.Create(user);
-		manager.Create(user);
-		manager.Create(user);
-		manager.editUserName(7,"7");
+		System.out.println("12312321");//		UserManager manager = new UserManager();
+//		manager.Create(user);
+//		manager.Create(user);
+//		manager.Create(user);
+//		manager.Create(user);
+//		manager.Create(user);
+//		manager.editUserName(7,"7");
 
 	}
 
